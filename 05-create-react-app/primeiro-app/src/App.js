08dcf7918c8bc7e35a938b9ae424fff416699000
Gustavo Loyola <guga.loyola17@gmail.com>
2023-07-05 18:11:25 -0300

    Props adicionadas no componente Card

--- 05-create-react-app/primeiro-app/src/App.js
+++ 05-create-react-app/primeiro-app/src/App.js
@@ -5,9 +5,9 @@ import Card from './components/card/card';
 function App() {
   return (
     <>
-    <Card/>
-    <Card/>
-    <Card/>
+    <Card title='Titulo card 1'/>
+    <Card title='Titulo card 2'/>
+    <Card title='Titulo card 3'/>
     </>
   );
 }
--- 05-create-react-app/primeiro-app/src/components/card/card.js
+++ 05-create-react-app/primeiro-app/src/components/card/card.js
@@ -1,15 +1,28 @@
 import React from "react";
 import './card.css'
 
-class Card extends React.Component {
-    render() {
-        return (
+// Passando propriedades através de um componente de função:
+
+const Card = (props) => {
+    return(
             <div className="card">
-                <h3>Título</h3>
+                <h3>{ props.title }</h3>
                 <p>Esse é um texto do card.</p>
             </div>
-        );
-    }
+    )
 }
+
+// Passando propriedades através de um componente de classe: 
+
+// class Card extends React.Component {
+//     render() {
+//         return (
+//             <div className="card">
+//                 <h3>{ this.props.title }</h3>
+//                 <p>Esse é um texto do card.</p>
+//             </div>
+//         );
+//     }
+// }
 
 export default Card
