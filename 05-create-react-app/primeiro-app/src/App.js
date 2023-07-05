a30e36f04674b203b70cbc350994d67125361a96
Gustavo Loyola <guga.loyola17@gmail.com>
2023-07-05 19:14:46 -0300

    Default props adicionado no componente Button

--- 05-create-react-app/primeiro-app/src/App.js
+++ 05-create-react-app/primeiro-app/src/App.js
@@ -1,24 +1,12 @@
 import './App.css';
-// import Button from './components/button/button';
-import Card from './components/card/card';
+import Button from './components/button/button';
+// import Card from './components/card/card';
 
 function App() {
   return (
     <>
-      <Card>
-        <h3>Titulo bem foda 🙂</h3>
-        <p>Esse é um texto do card</p>
-      </Card>
-
-      <Card>
-        <h3>Titulo mais ou menos 😐</h3>
-        <p>Esse é um texto do card 2</p>
-      </Card>
-
-      <Card>
-        <h3>Titulo ruim 😕</h3>
-        <p>Esse é um texto do card 3</p>
-      </Card>
+    <Button label='Baixar CV'/>
+    <Button/>
     </>
   );
 }
